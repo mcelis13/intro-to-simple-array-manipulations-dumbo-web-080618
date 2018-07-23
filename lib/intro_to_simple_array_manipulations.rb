b423c757b6423b446685e36bcfa6524ae94379f0
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 00:56:36 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,11 @@
 def using_push(array, string)
-  return array.push(string);
+  return array.push(string)
 end
 
 def using_unshift(array, string)
   return array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop()
 end
